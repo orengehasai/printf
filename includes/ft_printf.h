f2e0af29fd323099429ed2ffb7a28a6d02f4d736
かわ <qwedcxzas13579@gmail.com>
2025-05-05 18:26:05 +0900

    　

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: takenakatakeshiichirouta <takenakatakes    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/02 20:32:38 by takenakatak       #+#    #+#             */
-/*   Updated: 2025/05/05 11:44:08 by takenakatak      ###   ########.fr       */
+/*   Updated: 2025/05/05 18:18:47 by takenakatak      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,13 @@
 # include <stdarg.h>
 # include <stdint.h>
 
-int 	ft_printf(const char *, ...);
+int		ft_printf(const char *str, ...);
 
 int		print_c(char c);
 
 int		print_di(int n);
 
-int 	print_p(void *p);
+int		print_p(void *p);
 
 int		print_s(char *str);
 
@@ -32,8 +32,9 @@ int		print_u(unsigned int n);
 
 int		print_x(int n, char spe);
 
-void	put_hex(unsigned long int num, char *base, int *cnt, int *is_init_zeros);
+void	put_hex(unsigned long int num, char *base\
+				, int *cnt, int *is_init_zeros);
 
-void	put_nbr(long int num,int *cnt);
+void	put_nbr(long int num, int *cnt);
 
 #endif
--- src/print_s.c
+++ src/print_s.c
@@ -6,7 +6,7 @@
 /*   By: takenakatakeshiichirouta <takenakatakes    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/04 23:25:48 by takenakatak       #+#    #+#             */
-/*   Updated: 2025/05/05 17:10:34 by takenakatak      ###   ########.fr       */
+/*   Updated: 2025/05/05 18:24:40 by takenakatak      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,11 @@
 int	print_s(char *str)
 {
 	int	res;
+	char *nul;
 
+	nul = "(nil)";
 	if (!str)
-		return (write(1, "(null)", 6));
+		return (write(1, nul, ft_strlen(nul)));
 	res = ft_strlen(str);
 	return (write(1, str, res));
 }
